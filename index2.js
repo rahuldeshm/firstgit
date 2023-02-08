8a2863490b1a8791d104ece672ba1d3ecbe27a96
rahuldeshm <rahuldeshmukh454@gmail.com>
2023-02-08 18:46:34 +0530

    adding js file for third item green and all items bold

---
+++ index2.js
@@ -0,0 +1,7 @@
+// console.log("rahul")
+var items =document.getElementsByClassName("list-group-item")
+for (var i =0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
+// tit[0].style.fontWeight='bold';
+items[2].style.backgroundColor='green';
